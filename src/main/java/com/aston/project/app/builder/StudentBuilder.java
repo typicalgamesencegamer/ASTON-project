4c51b83ffc6571de7c896e7f4f9b0651f883d915
1dream4best <1dream4best@gmail.com>
2026-09-02 12:52:14 +0600

    RE: Реализация CustomClass

--- src/main/java/com/aston/project/app/builder/StudentBuilder.java
+++ src/main/java/com/aston/project/app/builder/StudentBuilder.java
@@ -7,11 +7,16 @@ public class StudentBuilder {
     private double minAverageGrade = 0.0;
     private double maxAverageGrade = 100.0;
 
+    private boolean groupNumberSet = false;
+    private boolean studentIdSet = false;
+    private boolean averageGradeSet = false;
+
     public StudentBuilder setGroupNumber(int groupNumber) {
         if (groupNumber <= 0) {
             throw new IllegalArgumentException("Номер группы должен быть положительным числом.");
         }
         this.groupNumber = groupNumber;
+        this.groupNumberSet = true;
         return this;
     }
 
@@ -20,6 +25,7 @@ public class StudentBuilder {
             throw new IllegalArgumentException("Номер зачетной книжки должен быть положительным числом.");
         }
         this.studentId = studentId;
+        this.studentIdSet = true;
         return this;
     }
 
@@ -28,10 +34,37 @@ public class StudentBuilder {
             throw new IllegalArgumentException("Средний балл должен быть в диапазоне от " + minAverageGrade + "до " + maxAverageGrade);
         }
         this.averageGrade = averageGrade;
+        this.averageGradeSet = true;
         return this;
     }
 
+    public int getGroupNumber() {
+        if (!groupNumberSet) throw new IllegalStateException("Group number not set.");
+        return groupNumber;
+    }
+
+    public int getStudentId() {
+        if (!studentIdSet) throw new IllegalStateException("Student ID not set.");
+        return studentId;
+    }
+
+    public double getAverageGrade() {
+        if (!averageGradeSet) throw new IllegalStateException("Average grade not set.");
+        return averageGrade;
+    }
+
+
     public Student build() {
+        if (!groupNumberSet) {
+            throw new IllegalStateException("Номер группы не был установлен.");
+        }
+        if (!studentIdSet) {
+            throw new IllegalStateException("Номер зачетной книжки не был установлен.");
+        }
+        if (!averageGradeSet) {
+            throw new IllegalStateException("Средний балл не был установлен.");
+        }
+
         return new Student(this);
     }
 }
