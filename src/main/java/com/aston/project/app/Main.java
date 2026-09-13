88c22795d9848ed5e6abed601847e2ba2b80fbb5
Alex Smirnov <sas2job@gmail.com>
2026-09-13 12:39:29 +0300

    feat: метод, подсчитывающий количество вхождений

--- src/main/java/com/aston/project/app/Main.java
+++ src/main/java/com/aston/project/app/Main.java
@@ -1,9 +1,17 @@
 package com.aston.project.app;
 
 import com.aston.project.app.core.Program;
+import com.aston.project.app.utils.multithreading.ElementOccurrenceCounter;
+
+import java.util.List;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
+        List<String> elements = List.of("Java", "SQL", "Java", "Spring", "Java", "Docker");
+        String searchedElement = "Java";
+        int occurrences = ElementOccurrenceCounter.countOccurrences(elements, searchedElement, 3);
+        System.out.printf("Количество вхождений элемента '%s': %d%n", searchedElement, occurrences);
+
         Program program = new Program();
         program.start();
     }
